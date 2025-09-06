c18cd209fafc4ba41e4a1f07bec01925b8f6e20c
Salehin-07 <mdabusalehin123@gmail.com>
2025-09-06 12:54:54 +0600

    updated psycopg

--- microtech-ltd/settings.py
+++ microtech-ltd/settings.py
@@ -164,7 +164,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = BASE_DIR / "staticfile"
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
